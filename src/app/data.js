a76b6b64ed3fab07f96f07bff88a62193de1669f
YuriyDz <dziapko.yurii@student.uzhnu.edu.ua>
2025-07-02 13:28:48 +0300

    add6

--- src/app/data.js
+++ src/app/data.js
@@ -1,33 +1,59 @@
-export const data =[{typeIm: "strategist",name: "ADAM WARLOCK", type: "Strategist", desc: "The genetically-engineered Adam Warlock wields mighty Quantum Magic, allowing him to connect and heal souls with a gentle touch. When the time comes for his allies to unite, Warlock emerges as the unwavering epicenter of cosmic justice!"},
-                    {typeIm: "dualist",name: "BLACK PANTHER", type: "Dualist", desc: "T'Challa, King of Wakanda, wields the perfect blend of the cutting-edge Vibranium technology and ancestral power drawn from the Panther God, Bast. The Black Panther bides his time until elegantly infiltrating enemy lines and commencing his hunt."},
-                 {typeIm: "vanguard",name: "VENOM", type: "Vanguard", desc: "T'Challa, King of Wakanda, wields the perfect blend of the cutting-edge Vibranium technology and ancestral power drawn from the Panther God, Bast. The Black Panther bides his time until elegantly infiltrating enemy lines and commencing his hunt."},
-                ];
-
-
+export const data = [
+  {
+    typeIm: "strategist",
+    name: "ADAM WARLOCK",
+    type: "Strategist",
+    desc: "The genetically-engineered Adam Warlock wields mighty Quantum Magic, allowing him to connect and heal souls with a gentle touch. When the time comes for his allies to unite, Warlock emerges as the unwavering epicenter of cosmic justice!",
+  },
+  {
+    typeIm: "dualist",
+    name: "BLACK PANTHER",
+    type: "Dualist",
+    desc: "T'Challa, King of Wakanda, wields the perfect blend of the cutting-edge Vibranium technology and ancestral power drawn from the Panther God, Bast. The Black Panther bides his time until elegantly infiltrating enemy lines and commencing his hunt.",
+  },
+  {
+    typeIm: "vanguard",
+    name: "VENOM",
+    type: "Vanguard",
+    desc: "T'Challa, King of Wakanda, wields the perfect blend of the cutting-edge Vibranium technology and ancestral power drawn from the Panther God, Bast. The Black Panther bides his time until elegantly infiltrating enemy lines and commencing his hunt.",
+  },
+];
 
 export const typesImages = {
   strategist: {
     icon: "https://www.marvelrivals.com/pc/gw/20241128194803/img/zy_clj_794208cf.png",
-    imagesLogo: "https://r.res.easebar.com/pic/20241120/79975fd7-91be-42ea-8edf-1c17d0522e09.png",
-    imageBackground: "https://r.res.easebar.com/pic/20241120/0f973982-df0d-47c8-96d2-cdf1f55563b9.png",
-    imageBackgroudBacker: "https://www.marvelrivals.com/pc/gw/20241128194803/img/bg_b0575d0e.jpg",
-    heros: "https://r.res.easebar.com/pic/20241120/02718499-b88c-4e31-83f8-e89ff802f4e1.png",
-    offset: {top: "20%", right: "40%", scale: 2}
+    imagesLogo:
+      "https://r.res.easebar.com/pic/20241120/79975fd7-91be-42ea-8edf-1c17d0522e09.png",
+    imageBackground:
+      "https://r.res.easebar.com/pic/20241120/0f973982-df0d-47c8-96d2-cdf1f55563b9.png",
+    imageBackgroudBacker:
+      "https://www.marvelrivals.com/pc/gw/20241128194803/img/bg_b0575d0e.jpg",
+    heros:
+      "https://r.res.easebar.com/pic/20241120/02718499-b88c-4e31-83f8-e89ff802f4e1.png",
+    offset: { top: "20%", right: "40%", scale: 2 },
   },
   dualist: {
     icon: "https://www.marvelrivals.com/pc/gw/20241128194803/img/zy_jdj_0a676db8.png",
-    imagesLogo: "https://r.res.easebar.com/pic/20241120/38064e9f-7efd-4faa-9e7d-124289425d46.png",
-    imageBackground: "https://r.res.easebar.com/pic/20241120/1c66c617-772e-4715-9fa8-163abaccd223.png",
-    imageBackgroudBacker: "https://www.marvelrivals.com/pc/gw/20241128194803/img/bg_b0575d0e.jpg",
-    heros: "https://r.res.easebar.com/pic/20241120/e721507b-764c-4206-9d93-f628314017e7.png",
-    offset: {top : "30%", right: "40%", scale: 2}
+    imagesLogo:
+      "https://r.res.easebar.com/pic/20241120/38064e9f-7efd-4faa-9e7d-124289425d46.png",
+    imageBackground:
+      "https://r.res.easebar.com/pic/20241120/1c66c617-772e-4715-9fa8-163abaccd223.png",
+    imageBackgroudBacker:
+      "https://www.marvelrivals.com/pc/gw/20241128194803/img/bg_b0575d0e.jpg",
+    heros:
+      "https://r.res.easebar.com/pic/20241120/e721507b-764c-4206-9d93-f628314017e7.png",
+    offset: { top: "30%", right: "40%", scale: 2 },
   },
   vanguard: {
     icon: "https://www.marvelrivals.com/pc/gw/20241128194803/img/zy_xfz_c18d1719.png",
-    imagesLogo: "https://r.res.easebar.com/pic/20241120/3e23b02a-f1e3-49c6-8f2c-10486fd4baa2.png",
-    imageBackground: "https://r.res.easebar.com/pic/20241120/8dcfb160-f06d-4d61-b16c-ac37e415c22b.png",
-    imageBackgroudBacker: "https://www.marvelrivals.com/pc/gw/20241128194803/img/bg_b0575d0e.jpg",
-    heros: "https://r.res.easebar.com/pic/20241120/b76153ef-8171-4a14-ad04-19af348085e1.png",
-    offset: {top: "30%", right: "35%", scale: 1.8}
-  }
+    imagesLogo:
+      "https://r.res.easebar.com/pic/20241120/3e23b02a-f1e3-49c6-8f2c-10486fd4baa2.png",
+    imageBackground:
+      "https://r.res.easebar.com/pic/20241120/8dcfb160-f06d-4d61-b16c-ac37e415c22b.png",
+    imageBackgroudBacker:
+      "https://www.marvelrivals.com/pc/gw/20241128194803/img/bg_b0575d0e.jpg",
+    heros:
+      "https://r.res.easebar.com/pic/20241120/b76153ef-8171-4a14-ad04-19af348085e1.png",
+    offset: { top: "30%", right: "35%", scale: 1.8 },
+  },
 };
